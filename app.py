15ff3f64cb4f86b60240831cbf2285b78aef465a
apurwa <apurwa@gmail.com>
2025-01-19 16:23:55 +0545

    new route

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ app=Flask(__name__)
 @app.route('/')
 def webout():
 	return '<h1>DevOps is fun.</h1>'
+
+@app.route('/greet')
+def greet():
+	return '<h1>New feature</h1>'
 app.run(host='0.0.0.0',port=7000)
