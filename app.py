d75b31eb59a9ef4e3400cf6b95d126932db41c30
shrisanth <shreeojha89@gmail.com>
2025-01-23 12:25:41 +0545

    python repo

--- app.py
+++ app.py
@@ -3,8 +3,4 @@ app=Flask(__name__)
 @app.route('/')
 def webout():
 	return '<h1>DevOps is fun.</h1>'
-
-@app.route('/greet')
-def greet():
-	return '<h1>New feature is great</h1>'
 app.run(host='0.0.0.0',port=7000)
